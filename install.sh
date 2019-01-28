9770577d602f5bf2688e82fc66342213630cc26e
Rahul Prasad <therahulprasad@users.noreply.github.com>
2019-01-28 14:30:02 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+$ Backup everything
+ls $HOME/.vimrc && (cp $HOME/.vimrc $HOME/.vimrc_backup || (echo "Backup of .vimrc failed. Exiting."; exit 1)) || (echo "~/.vimrc not found continuing")
+ls $HOME/.vim && (cp -r $HOME/.vim $HOME/.vim_backup || (echo "Backup of .vim failed. Exiting."; exit 1)) || (echo "~/.vim forder not found continuing")
+
+# This is dangereous but git clone wont work if directory is not clean
+rm -rf $HOME/.vim && mkdir $HOME/.vim
+cd $HOME/.vim
+git clone git@github.com:rahulpache/my-vim-configulation.git .
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+pip install powerline-status
+git submodule init
+git submodule update
+cd $HOME/.vim/bundle/sparkup
+make vim-pathogen
