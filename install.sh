ba3d6a9dcb9a51141ab476f22cf80756b76c9c85
Rahul Prasad <therahulprasad@users.noreply.github.com>
2019-01-29 17:47:28 +0530

    fix dependencies installation

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 ls $HOME/.vimrc && (cp $HOME/.vimrc $HOME/.vimrc_backup || (echo "Backup of .vimrc failed. Exiting."; exit 1)) || (echo "~/.vimrc not found continuing")
 ls $HOME/.vim && (cp -r $HOME/.vim $HOME/.vim_backup || (echo "Backup of .vim failed. Exiting."; exit 1)) || (echo "~/.vim forder not found continuing")
 
+# Make sure needed packages are installed
+sudo apt-get install -y exuberant-ctags python3-pip
+
 # This is dangereous but git clone wont work if directory is not clean
 rm -rf $HOME/.vim && mkdir $HOME/.vim
 cd $HOME/.vim
 git clone https://github.com/therahulprasad/my-vim-configulation.git .
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
-pip install powerline-status
+pip3 install powerline-status
 git submodule init
 git submodule update
 cd $HOME/.vim/bundle/sparkup
